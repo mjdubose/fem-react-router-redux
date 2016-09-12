73085344666859dcd17a09040c8374c1fe1647f0
mjdubose <matthew.dubose@gmail.com>
2016-09-12 10:34:49 -0500

    added layout.jsx for background styling

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,13 +2,16 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./landing')
 const Search = require('./search')
+const Layout = require('./layout')
 const ReactRouter = require('react-router')
-const {Router, Route, hashHistory} = ReactRouter
+const {Router, Route, IndexRoute, hashHistory} = ReactRouter
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path ='/' component={Landing} />
+  <Route path ='/' component = {Layout}>
+    <IndexRoute component={Landing} />
     <Route path='/search' component={Search}/>
+    </Route>
   </Router>
 )
 
--- js/landing.jsx
+++ js/landing.jsx
@@ -1,15 +1,12 @@
 const React = require('react')
 const {Link} = require('react-router')
 
-const Landing = () => (
-
-   <div className='app-container'>
+const Landing = () => ( 
     <div className='home-info'>
      <h1 className='title'>svideo</h1>
-     <input className='search' type='text' placeholder='Search' />
-     <Link to='/search' className='browse-all'> or Browse All </Link>
-    </div>
-   </div>
+       <input className='search' type='text' placeholder='Search' />
+       <Link to='/search' className='browse-all'> or Browse All </Link>
+    </div> 
     
  )
 
